f25502d2637abad37aba70e286535da7a6f7c3ff
DeJohn Nelson <dejohn.nelson1998@gmail.com>
2020-09-16 20:29:41 -0500

    things are working so far

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
 // Write your JavaScript code here!
 
-/* This block of code shows how to format the HTML once you fetch some planetary JSON!
-<h2>Mission Destination</h2>
-<ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
-</ol>
-<img src="${}">
-*/
+// This block of code shows how to format the HTML once you fetch some planetary JSON!
+
+
 window.addEventListener("load", function() {
+   fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
+   response.json().then( function(json) {
+          const div = document.getElementById("missionTarget");
+         
+          div.innerHTML = `
+          <h2>Mission Destination</h2>
+<ol>
+   <li>Name: ${json[0].name}</li>
+   <li>Diameter: ${json[0].diameter}</li>
+   <li>Star: ${json[0].star}</li>
+   <li>Distance from Earth: ${json[0].distance}</li>
+   <li>Number of Moons: ${json[0].moons}</li>
+</ol>
+<img src="${json[0].image}">
+            `;
+      }) 
+   })
    let form = document.querySelector("form");
    form.addEventListener("submit", function(event) {
        let pilotNameInput = document.querySelector("input[name=pilotName]");
@@ -21,7 +30,9 @@ window.addEventListener("load", function() {
        if(isNaN(fuelLevelInput) || isNaN(cargoMassInput) || copilotName === "" || pilotNameInput === "") {
            window.alert("All fields are required");
            event.preventDefault();
-       }   
+
+         }   
+
    })
 })
 
@@ -46,13 +57,3 @@ window.addEventListener("load", function() {
    //      });
 
 
-fetch("file:///C:/Users/dejoh/LC101/launchcodechecklist/Launch-Checklist-Form/index.html").then(function(response) {
-      response.json().then( function(json) {
-          const div = document.getElementById("faultyItems");
-          div.innerHTML = `
-          <ul>
-            <li>pilotStatus${json.pilotStatus}</li>
-            <li>copilotStatus${json.copilotStatus}</li>
-            </ul>
-            `;
-      }) })
